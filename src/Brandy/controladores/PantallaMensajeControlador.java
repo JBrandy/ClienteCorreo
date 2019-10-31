cba50266f18d299d3cc0e5559678363ee0eaef96
J.Brandy <jesusbrandygarcia@gmail.com>
2019-10-31 18:04:47 +0100

    Diseño

---
+++ src/Brandy/controladores/PantallaMensajeControlador.java
@@ -0,0 +1,43 @@
+package Brandy.controladores;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class PantallaMensajeControlador {
+
+
+    public class PleaseProvideControllerClassName {
+
+        @FXML
+        private TextField tfPara;
+
+        @FXML
+        private TextField tfCc;
+
+        @FXML
+        private TextField tfAsunto;
+
+        @FXML
+        private Button btEnviar;
+
+        @FXML
+        private TextField tfDe;
+
+        @FXML
+        private Button btCancelar;
+
+        @FXML
+        void cancelar(ActionEvent event) {
+
+        }
+
+        @FXML
+        void enviar(ActionEvent event) {
+
+        }
+
+    }
+}
