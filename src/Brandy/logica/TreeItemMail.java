614bc97867a34bcf7052414e088ea3af61727488
JBrandy <jesusbrandygarcia@gmail.com>
2019-11-11 17:25:06 +0100

    TreeItem V2

---
+++ src/Brandy/logica/TreeItemMail.java
@@ -0,0 +1,39 @@
+package Brandy.logica;
+
+import javafx.collections.ObservableList;
+import javafx.scene.control.TreeItem;
+
+import javax.mail.Folder;
+import java.io.File;
+
+public class TreeItemMail extends TreeItem<String> {
+  private String nombre;
+  private UsuarioCorreo user;
+  private Folder[] folder;
+
+  public TreeItemMail(String nombre, UsuarioCorreo user, Folder[] folder) {
+    super(nombre);
+
+    this.nombre = nombre;
+    this.user = user;
+    this.folder = folder;
+  }
+
+
+
+ /* private ObservableList<TreeItem<String>> buildChildren(TreeItem<String> TreeItem) {
+    File f = new File(TreeItem.getValue());
+    if (f != null && f.isDirectory()) {
+      File[] files = f.listFiles();
+      if (files != null) {
+        ObservableList<TreeItem<File>> children = FXCollections.observableArrayList();
+        for (File childFile : files) {
+          children.add(createNode(childFile));
+        }
+        return children;
+      }
+    }
+    return FXCollections.emptyObservableList();
+  }*/
+
+}
